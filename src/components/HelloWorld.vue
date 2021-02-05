0f80f279eb359387b49bf655a6855768368a9566
wkai2573 <wkai2573@gmail.com>
2021-02-05 12:25:29 +0800

    1. 更新7kai好帥

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <p>
 			(。-ω-)zzz. . . (。ﾟωﾟ) ﾊｯ!
+			7kai 好帥
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
